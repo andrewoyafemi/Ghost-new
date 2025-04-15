90ca331e910566687a57f5c36e9683a873b46880
andrewoyafemi <andrewoyafemi@gmail.com>
2025-04-15 16:25:43 +0100

    redis config

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,13 +1,23 @@
 import Redis from "ioredis";
 import logger from "./logger";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Create a Redis client
-const redisClient = new Redis({
-  host: process.env.REDIS_HOST || "localhost",
-  port: parseInt(process.env.REDIS_PORT || "6379"),
-  password: process.env.REDIS_PASSWORD || undefined,
-});
+// const redisClient = new Redis({
+//   host: process.env.REDIS_HOST || "localhost",
+//   port: parseInt(process.env.REDIS_PORT || "6379"),
+//   password: process.env.REDIS_PASSWORD || undefined,
+// });
 
+// FOR RENDER
+const redisClient = new Redis(
+  process.env.REDIS_URL || "redis://red-cvv7ccidbo4c73fhcd30:6379"
+);
+
+redisClient.set("ghostryt:status", "active");
+redisClient.get("ghostryt:status").then(console.log);
 // Log connection events
 redisClient.on("connect", () => {
   logger.info("Connected to Redis");
